83bcc091826863e9377a004b9d0ddc3130095265
Mark Momot <markmomot03@gmail.com>
2022-04-06 15:24:56 -0400

    commit tree 36813c8b539670710da406af6912f377181c6318

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :no_wrong_title
+
+    def no_wrong_title
+        if title == "True Facts"
+            errors.add(:title, "Sorry, wrong title")
+        end
+    end
 end
